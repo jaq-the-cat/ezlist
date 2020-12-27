1ea79bef4f4e79817b046d0c24341aa66a53a3fa
jaq-cat <jaq.cat@protonmail.ch>
2020-12-26 21:14:08 -0300

    pog

---
+++ lib/mainpage.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class ListStuff extends StatefulWidget {
+    ListStuff({Key key, this.title}) : super(key: key);
+
+    final String title;
+
+    @override
+    _ListStuffState createState() => _ListStuffState();
+}
+
+class _ListStuffState extends State<ListStuff> {
+    List<Thingy> _stuff = [];
+
+    void _incrementCounter() {
+        setState(() {
+            _stuff.add(Thingy(content: "Item " + (_stuff.length+1).toString()));
+        });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text(widget.title),
+            ),
+            body: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.all(25),
+                    child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: _stuff,
+                        ),
+                    )
+                ),
+            ),
+            floatingActionButton: FloatingActionButton(
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+    }
+}
+
+class Thingy extends StatelessWidget {
+    Thingy({Key key, this.content}) : super(key: key);
+
+    final String content;
+
+    @override
+    Widget build(BuildContext context) {
+        const double r = 12;
+        return Container(
+            margin: EdgeInsets.only(top: r),
+            decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                borderRadius: BorderRadius.circular(r),
+            ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                    Container(
+                        padding: EdgeInsets.all(r - 2),
+                        child: Text(
+                            content,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 22,
+                            ),
+                        ),
+                        alignment: Alignment.centerLeft,
+                    ),
+                    Container(
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.delete),
+                            ),
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.only(topRight: Radius.circular(r), bottomRight: Radius.circular(r)),
+                            ),
+                    ),
+                ]
+            )
+        );
+    }
+}
