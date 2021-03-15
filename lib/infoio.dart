d787526c41b539c9e0945761bf0e35981ec649f1
jaq-cat <jaq.cat@protonmail.ch>
2021-03-15 10:53:29 -0300

    useless import

--- lib/infoio.dart
+++ lib/infoio.dart
@@ -1,6 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 Future<void> addItem(String text) async {
     final prefs = await SharedPreferences.getInstance();
